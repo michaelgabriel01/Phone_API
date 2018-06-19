13c8a28c5def652dc54ac3b6f7e943201894b6b0
MichaelJames02 <michael4achieve@gmail.com>
2018-06-19 20:18:05 -0300

    Registered employee class in the admin

--- CallApi/admin.py
+++ CallApi/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Imorts from local app(s)
-from .models import Staff, Company, Empressa, Album, Call, Track
+from .models import Staff, Company, Empressa, Album, Call, Track, EmployeeSerializer
 
 admin.site.register(Staff)
 admin.site.register(Company)
@@ -9,3 +9,4 @@ admin.site.register(Empressa)
 admin.site.register(Call)
 admin.site.register(Album)
 admin.site.register(Track)
+admin.site.register(EmployeeSerializer)
